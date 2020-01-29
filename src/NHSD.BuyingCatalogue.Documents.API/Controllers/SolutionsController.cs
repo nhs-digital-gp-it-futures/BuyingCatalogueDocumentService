7ae3765aff9223275a82ae2b22035ccad171f1d9
Callum Bradbury <callum.bradbury@bjss.com>
2020-01-29 15:43:46 +0000

    3657: Added Documents.API project with swagger & stubbed out SolutionsController

---
+++ src/NHSD.BuyingCatalogue.Documents.API/Controllers/SolutionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NHSD.BuyingCatalogue.Documents.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [AllowAnonymous]
+    public class SolutionsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}/documents/{type}/exists")]
+        public ActionResult Exists(string id, string type)
+        {
+            if (id == "42")
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ src/NHSD.BuyingCatalogue.Documents.API/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace NHSD.BuyingCatalogue.Documents.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+            => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Documents API",
+                    Version = "v1",
+                    Description = "NHS Digital GP IT Buying Catalogue Documents HTTP API"
+                }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage()
+                    .UseSwagger()
+                    .UseSwaggerUI(options =>
+                    {
+                        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Buying Catalogue Documents API V1");
+                    });
+            }
+
+            app.UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+        }
+    }
+}
