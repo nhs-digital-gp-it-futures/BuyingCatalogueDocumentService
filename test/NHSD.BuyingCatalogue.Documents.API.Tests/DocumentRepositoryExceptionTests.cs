aecaa6515d860ed551d7f70b3bdc72bfb3a1be7e
bjss-chris <christopher.martin@bjss.com>
2020-02-06 12:30:13 +0000

    Make tests parallelizable
    Refactor the Azure repository tests to allow them to run in parallel and marked all test fixtures as parallelizable.
    
    AB#4848

--- test/NHSD.BuyingCatalogue.Documents.API.Tests/DocumentRepositoryExceptionTests.cs
+++ test/NHSD.BuyingCatalogue.Documents.API.Tests/DocumentRepositoryExceptionTests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 namespace NHSD.BuyingCatalogue.Documents.API.UnitTests
 {
     [TestFixture]
+    [Parallelizable(ParallelScope.All)]
     internal sealed class DocumentRepositoryExceptionTests
     {
         [Test]
