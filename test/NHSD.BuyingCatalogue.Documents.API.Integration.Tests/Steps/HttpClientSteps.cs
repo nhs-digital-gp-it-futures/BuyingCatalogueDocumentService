19870654f4b7a6dc7946ef1b81163a2778ca795d
Adrian Soomro <adrian.soomro@gmail.com>
2020-02-10 15:14:26 +0000

    3657: Download file integration tests

--- test/NHSD.BuyingCatalogue.Documents.API.Integration.Tests/Steps/HttpClientSteps.cs
+++ test/NHSD.BuyingCatalogue.Documents.API.Integration.Tests/Steps/HttpClientSteps.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace NHSD.BuyingCatalogue.Documents.API.IntegrationTests.Steps
     [Binding]
     internal class HttpClientSteps
     {
+        private const string SampleDataPath = "SampleData";
         private readonly ScenarioContext _context;
         private readonly AzureBlobStorageScenarioContext _azureBlobStorageScenarioContext;
 
@@ -41,6 +43,17 @@ namespace NHSD.BuyingCatalogue.Documents.API.IntegrationTests.Steps
             _context["Response"] = response;
         }
 
+        [When("a GET (.*) document request is made for solution (.*)")]
+        public async Task GetDocumentAsStreamForSolution(string fileName, string solutionId)
+        {
+            using var client = new HttpClient();
+
+            var slnId = _azureBlobStorageScenarioContext.TryToGetGuidFromSolutionId(solutionId);
+            var response = await client.GetAsync(new Uri($"{_context["RootUrl"]}/{slnId}/documents/{fileName}"))
+                .ConfigureAwait(false);
+            _context["Response"] = response;
+        }
+
         [Then(@"a response with status code ([\d]+) is returned")]
         public void AResponseIsReturned(int code)
         {
@@ -59,6 +72,25 @@ namespace NHSD.BuyingCatalogue.Documents.API.IntegrationTests.Steps
 
             var content = JToken.Parse(await response.Content.ReadAsStringAsync());
             content.Select(t => t.Value<string>()).Should().BeEquivalentTo(elements.FileNames);
+        }
+
+        [Then(@"the content of the response is equal to (.*) belonging to (.*)")]
+        public async Task ContentOfTheResponseIsEqualTo(string fileName, string solutionId)
+        {
+            var response = _context["Response"] as HttpResponseMessage;
+            response.Should().NotBeNull();
+
+            var responseBytes = await GetBytesFromStream(await response.Content.ReadAsStreamAsync());
+            var ourFileBytes = await GetBytesFromStream(File.OpenRead(Path.Combine(SampleDataPath, solutionId, fileName)));
+
+            responseBytes.Should().BeEquivalentTo(ourFileBytes);
+        }
+
+        private async Task<byte[]> GetBytesFromStream(Stream stream)
+        {
+            var resultBytes = new byte[stream.Length];
+            await stream.ReadAsync(resultBytes, 0, (int)stream.Length);
+            return resultBytes;
         }
 
         private class FileTable
